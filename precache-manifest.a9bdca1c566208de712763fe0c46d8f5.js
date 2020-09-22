7ed847f44fd3bb5609fc43347c97020cef2aa6df
李白白 <soficat131@163.com>
2020-09-22 11:45:28 +0800

    first submit

---
+++ precache-manifest.a9bdca1c566208de712763fe0c46d8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9ef37d0f01ca90e730a8a0c0ae92e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f981f7f80b289c2f723a",
+    "url": "./static/css/main.9bd6ead3.chunk.css"
+  },
+  {
+    "revision": "6aea41900da39f0160ed",
+    "url": "./static/js/2.a6c940f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a6c940f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f981f7f80b289c2f723a",
+    "url": "./static/js/main.b3cc3fde.chunk.js"
+  },
+  {
+    "revision": "5f5f98b003250ff108a9",
+    "url": "./static/js/runtime-main.04e1fd0e.js"
+  }
+]);
